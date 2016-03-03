2b91d30c7aa96419bf28f794751c3d0478101e16
mvoloder <marko.voloder@gmail.com>
2016-03-03 18:47:13 +0100

    bgng

---
+++ add.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>Pocetak php-a</title></head>
+<body>
+
+<?php
+ echo "<h2> php program<h2></br>";
+ $broj_1 = 10;
+ $broj_2 = 20;
+ $suma = $broj_2 + $broj_1;
+ echo "rezultat je : $suma";
+
+?>
+
+</body>
+</html>
